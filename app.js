d98567b9c3f43f81a750cecb7cb64bd244b3d8c8
vishmapratimdashere <bhishmadas9@gmail.com>
2025-11-12 03:35:06 -0500

    Added favicon logic

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
 const path=require('path')
+const favicon=require('serve-favicon')
 
 app.set('view engine','ejs')
 app.set('views', path.join(__dirname,"views"))
 app.use(express.static("public")); 
 // use a middleware before everything else; make the 'public' folder publicly accessible
+
+app.use(favicon(path.join(__dirname,'/public','images/Favicon.png')))
 
 app.get(
     '/', (req,res)=> {
